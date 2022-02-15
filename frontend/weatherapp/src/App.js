7e84505e392ec7dfa92b598f476a9a71b7367931
Poorva <poorva@Poorvas-MacBook-Air.local>
2022-02-15 12:54:05 +0530

    first cut ready with no css

--- frontend/weatherapp/src/App.js
+++ frontend/weatherapp/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import Home from './screens/Home/Home';
 
 function App() {
--- frontend/weatherapp/src/components/Body/Body.js
+++ frontend/weatherapp/src/components/Body/Body.js
@@ -2,7 +2,7 @@ import React, {useState}  from 'react'
 
 function Body() {
     const [city, setCity] = useState("")
-    const [forecasts, setForeCasts] = useState([])
+    const [forecasts, setForeCasts] = useState({})
     
     const cityChangeHandler = (e) => setCity(e.target.value)  
     
@@ -10,19 +10,24 @@ function Body() {
         if(city !== ""){
             fetch(`http://localhost:8000/${city}`)             
             .then(res => {
-                return res.json() 
-                // setForeCasts(res)               
+                return res.json()              
             })
             .then(data => {               
-                setForeCasts(data)            
+                setForeCasts(data)                      
             })
             .catch(err => console.log(err))
-        }
+        }        
       }
-      
-      
 
-
+    //   console.log(forecasts.consolidated_weather[0] );
+    
+    //   let minTemp = Math.round(forecasts.consolidated_weather[0].min_temp * 10) / 10
+    //   let maxTemp = Math.round(forecasts.consolidated_weather[0].max_temp * 10) / 10
+    //   let aggrTemp = Math.round(forecasts.consolidated_weather[0].the_temp * 10) / 10
+    //   let windSpeed = Math.round(forecasts.consolidated_weather[0].wind_speed * 10) / 10
+    //   let visibility = Math.round(forecasts.consolidated_weather[0].visibility * 10) / 10
+    //   let airPressure = forecasts.consolidated_weather[0].airpressure
+      
   return (
     <div>
         <div>
@@ -32,12 +37,27 @@ function Body() {
             <input type="text" placeholder="Enter City Name"onChange={cityChangeHandler}/>
             <button type="button" onClick={cityClickHandler}>Enter</button>
         </div>
-        <div>        
-            {forecasts && (
-                items.map((item) => {
-                    console.log(item);
-                })
-            )}
+        <div> 
+            {
+                forecasts && (
+                    <>
+                    <h3>City: {city}</h3>
+                    <p>Min Tempreature: {forecasts.consolidated_weather[0].min_temp}</p>   
+                    <p>Max Tempreature: {forecasts.consolidated_weather[0].max_temp}</p>
+                    <p>Aggr Tempreature: {forecasts.consolidated_weather[0].the_temp}</p>
+                    <p>Wind Speed: {forecasts.consolidated_weather[0].wind_speed}</p>
+                    <p>Visibility: {forecasts.consolidated_weather[0].visibility}</p>
+                    <p>Air Pressure: {forecasts.consolidated_weather[0].air_pressure}</p>   
+                    </>
+                )
+            }
+
+            {
+                !forecasts && (
+                    <h3>Data not found</h3>
+                )
+            }
+                    
         </div>
     </div>
   )
